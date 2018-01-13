3f1c12f04df6d3e002be23684318f990c82f7541
Pierfrancesco Soffritti <soffritti.pierfrancesco@gmail.com>
2018-01-13 11:13:48 +0100

    added MediaRouterButton to YouTubePlayerView

--- app/src/main/java/com/pierfrancescosoffritti/chromecastyoutubesample/MainActivity.kt
+++ app/src/main/java/com/pierfrancescosoffritti/chromecastyoutubesample/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.pierfrancescosoffritti.chromecastyoutubesample
 
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.app.MediaRouteButton
+import android.view.View
 import com.google.android.gms.cast.framework.*
 import com.pierfrancescosoffritti.chromecastyoutubesample.chromecast.ChromecastContainer
 import com.pierfrancescosoffritti.chromecastyoutubesample.chromecast.ChromecastCommunicationChannel
@@ -10,6 +13,12 @@ import com.pierfrancescosoffritti.chromecastyoutubesample.youTube.chromecast.Chr
 import com.pierfrancescosoffritti.chromecastyoutubesample.youTube.MyYouTubePlayerListener
 import com.pierfrancescosoffritti.youtubeplayer.player.YouTubePlayerInitListener
 import kotlinx.android.synthetic.main.activity_main.*
+import com.pierfrancescosoffritti.youtubeplayer.player.AbstractYouTubePlayerListener
+import android.content.res.TypedArray
+import android.graphics.drawable.Drawable
+import android.support.v4.graphics.drawable.DrawableCompat
+import android.view.ContextThemeWrapper
+
 
 class MainActivity : AppCompatActivity(), ChromecastContainer {
 
@@ -19,34 +28,76 @@ class MainActivity : AppCompatActivity(), ChromecastContainer {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        chromeCastYouTubePlayer = ChromecastYouTubePlayer()
-
-        initChromecast()
-
         initUI()
+        initYouTubePlayers()
+        val mediaRouterButton = initChromecast()
     }
 
     private fun initUI() {
-        play_button.setOnClickListener({chromeCastYouTubePlayer.play()})
-        pause_button.setOnClickListener({chromeCastYouTubePlayer.pause()})
+        chromecast_controls.visibility = View.GONE
     }
 
     override fun onSessionStarting() {
-        // update UI
+        youtube_player_view.visibility = View.GONE
+        chromecast_controls.visibility = View.VISIBLE
     }
 
     override fun onSessionEnding() {
-        // update UI
+        youtube_player_view.visibility = View.VISIBLE
+        chromecast_controls.visibility = View.GONE
     }
 
     override fun setCommunicationChannel(communicationChannelChromecast: ChromecastCommunicationChannel) {
         chromeCastYouTubePlayer.initialize(communicationChannelChromecast, YouTubePlayerInitListener { it.addListener(MyYouTubePlayerListener(it)) })
     }
 
-    private fun initChromecast() {
+    private fun initYouTubePlayers() {
+        initChromecastYouTubePlayer()
+        initLocalYouTube()
+    }
+
+    private fun initChromecastYouTubePlayer() {
+        chromeCastYouTubePlayer = ChromecastYouTubePlayer()
+    }
+
+    private fun initLocalYouTube() {
+        this.lifecycle.addObserver(youtube_player_view)
+
+        youtube_player_view.initialize({ youTubePlayer ->
+
+            youTubePlayer.addListener(object : AbstractYouTubePlayerListener() {
+                override fun onReady() {
+                    val videoId = "6JYIGclVQdw"
+                    youTubePlayer.loadVideo(videoId, 0f)
+                }
+            })
+
+        }, true)
+    }
+
+    private fun initChromecast() : MediaRouteButton {
         val chromecastManager = ChromecastManager(this, this)
         lifecycle.addObserver(chromecastManager)
 
-        CastButtonFactory.setUpMediaRouteButton(applicationContext, media_route_button)
+        val mediaRouterButton = initMediaRouterButton()
+
+        return mediaRouterButton
+    }
+
+    private fun initMediaRouterButton() : MediaRouteButton {
+        val mediaRouterButton = MediaRouteButton(this)
+
+        val castContext = ContextThemeWrapper(this, android.support.v7.mediarouter.R.style.Theme_MediaRouter)
+        val a = castContext.obtainStyledAttributes(null, android.support.v7.mediarouter.R.styleable.MediaRouteButton, android.support.v7.mediarouter.R.attr.mediaRouteButtonStyle, 0)
+        val drawable = a.getDrawable(android.support.v7.mediarouter.R.styleable.MediaRouteButton_externalRouteEnabledDrawable)
+        a.recycle()
+        DrawableCompat.setTint(drawable, ContextCompat.getColor(this, android.R.color.white))
+
+        mediaRouterButton.setRemoteIndicatorDrawable(drawable)
+
+        youtube_player_view.playerUIController.addView(mediaRouterButton)
+        CastButtonFactory.setUpMediaRouteButton(applicationContext, mediaRouterButton)
+
+        return mediaRouterButton
     }
 }
