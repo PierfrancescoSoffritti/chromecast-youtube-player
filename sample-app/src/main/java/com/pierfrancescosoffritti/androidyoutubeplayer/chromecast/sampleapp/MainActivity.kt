c0a0ef7a6567d5a31ba7d579411eb5f178db2e5e
PierfrancescoSoffritti <soffritti.pierfrancesco@gmail.com>
2018-06-22 22:25:24 +0200

    notification example base structure

--- sample-app/src/main/java/com/pierfrancescosoffritti/androidyoutubeplayer/chromecast/sampleapp/MainActivity.kt
+++ sample-app/src/main/java/com/pierfrancescosoffritti/androidyoutubeplayer/chromecast/sampleapp/MainActivity.kt
@@ -14,6 +14,7 @@ import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.view.WindowManager
+import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import com.getkeepsafe.taptargetview.TapTarget
